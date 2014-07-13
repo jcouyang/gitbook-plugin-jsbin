5ac808e8870466a657988f1e25a84522ff73e1f6
Jichao Ouyang <oyanglulu@gmail.com>
2014-07-13 13:32:11 +0800

    auto embedded jsbin link

--- book/plugin.js
+++ book/plugin.js
@@ -1,9 +1,13 @@
-require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function(e, config) {
-        (function() {
-            var jsbin = document.createElement('script'); jsbin.type = 'text/javascript'; jsbin.async = true;
-            jsbin.src = '//static.jsbin.com/js/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(jsbin);
-        })();
-    });
+require(["gitbook", "jquery"], function(gitbook, $) {
+  gitbook.events.bind("page.change", function(e, config) {
+    (function() {
+			$("a").each(function(link){
+				if(link.href && link.href.match(/jsbin.com\/.*embed/))
+					$(link).addClass("jsbin-embed");
+			});
+      var jsbin = document.createElement('script'); jsbin.type = 'text/javascript'; jsbin.async = true;
+      jsbin.src = '//static.jsbin.com/js/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(jsbin);
+    })();
+  });
 });
